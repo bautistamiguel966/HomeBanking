91805429fc6afb78cd56666a2d5eff5cc41c7b9d
bautistamiguel966 <miguelbautista182@gmail.com>
2023-08-03 10:52:13 -0300

    Fin de la task1

--- src/main/java/com/ap/homebanking/HomebankingApplication.java
+++ src/main/java/com/ap/homebanking/HomebankingApplication.java
@@ -1,13 +1,27 @@
 package com.ap.homebanking;
 
+import com.ap.homebanking.models.Client;
+import com.ap.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository) {
+		return (args) -> {
+
+			clientRepository.save(new Client("Melba", "Morel", "melba@mindhub.com"));
+			clientRepository.save(new Client("Rocio", "Diaz", "rocio@mindhub.com"));
+
+		};
 	}
 
 }
