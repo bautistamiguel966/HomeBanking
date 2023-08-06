d7668606418008a863a4eb5c23c01b5d89cb136d
bautistamiguel966 <miguelbautista182@gmail.com>
2023-08-06 19:46:00 -0300

    Se termino la task2

--- src/main/java/com/ap/homebanking/HomebankingApplication.java
+++ src/main/java/com/ap/homebanking/HomebankingApplication.java
@@ -1,11 +1,14 @@
 package com.ap.homebanking;
 
+import com.ap.homebanking.models.Account;
 import com.ap.homebanking.models.Client;
+import com.ap.homebanking.repositories.AccountRepository;
 import com.ap.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -15,11 +18,33 @@ public class HomebankingApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository) {
+	public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository) {
 		return (args) -> {
 
-			clientRepository.save(new Client("Melba", "Morel", "melba@mindhub.com"));
-			clientRepository.save(new Client("Rocio", "Diaz", "rocio@mindhub.com"));
+			//clientRepository.save(new Client("Melba", "Morel", "melba@mindhub.com"));
+			//clientRepository.save(new Client("Rocio", "Diaz", "rocio@mindhub.com"));
+
+			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com");
+			Client client2 = new Client("Rocio", "Diaz", "rocio@mindhub.com");
+
+			Account account1 = new Account("VIN001", LocalDate.now(), 5000);
+			Account account2 = new Account("VIN002", LocalDate.now().plusDays(1), 7500);
+			Account account3 = new Account("VIN003", LocalDate.now().plusDays(3), 4000);
+			Account account4 = new Account("VIN004", LocalDate.now().plusDays(7), 9000);
+
+			client1.addAccount(account1);
+			client1.addAccount(account2);
+
+			client2.addAccount(account3);
+			client2.addAccount(account4);
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+
+			accountRepository.save(account1);
+			accountRepository.save(account2);
+			accountRepository.save(account3);
+			accountRepository.save(account4);
 
 		};
 	}
--- src/main/java/com/ap/homebanking/models/Client.java
+++ src/main/java/com/ap/homebanking/models/Client.java
@@ -2,10 +2,9 @@ package com.ap.homebanking.models;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Client {
@@ -18,6 +17,9 @@ public class Client {
     private String lastName;
     private String email;
 
+    @OneToMany(mappedBy="client", fetch= FetchType.EAGER)
+    Set<Account> accounts = new HashSet<>();
+
     public Client(){}
 
     public Client(String firstName, String lastName, String email) {
@@ -26,6 +28,9 @@ public class Client {
         this.email = email;
     }
 
+    public long getId() {
+        return id;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -49,5 +54,14 @@ public class Client {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(Account account) {
+        account.setClient(this);
+        accounts.add(account);
     }
 }
